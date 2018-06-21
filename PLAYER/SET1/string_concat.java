c02a518fbaa91349f1872d2d9be57ba6d0e34c40
ANUMUNIRAJU <34641587+ANUMUNIRAJU@users.noreply.github.com>
2018-06-21 12:35:49 +0530

    Create string_concat.java

---
+++ PLAYER/SET1/string_concat.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class JavaApplication1 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+          String s=sc.nextLine();
+          
+        System.out.println(s+".");
+    }
+    
+}
